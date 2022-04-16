598e48a10f0fd09059158474d241406636112887
artawower <artawower@protonmail.com>
2022-04-16 19:58:52 +0300

    feature: init dependencies

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { readFileSync, statSync } from "fs";
+import { collectNoteFromFile, collectNotesFromDir } from "second-brain-parser";
+import { Note } from "second-brain-parser/dist/parser";
+
+interface SecondBrainPublishedConfig {
+  remoteAddress: string;
+  token: string;
+  baseDir: string;
+}
+
+const defaultUrl =
+  process.env.SECOND_BRAIN_SERVER_URL || "https://second-brain.org";
+
+const configPath =
+  process.env.SECOND_BRAIN_CONFIG_PATH || "~/.config/second-brain/config.json";
+
+const baseDir = process.env.SECOND_BRAIN_BASE_DIR || "";
+
+const readConfig = (path: string): SecondBrainPublishedConfig => {
+  try {
+    return JSON.parse(readFileSync(path).toString());
+  } catch (e) {
+    return {
+      remoteAddress: defaultUrl,
+      token: process.env.SECOND_BRAIN_TOKEN || "",
+      baseDir,
+    };
+  }
+};
+
+// TODO: master promise axios
+const sendNotes = (notes: Note[]): void => {
+  console.log("🦄: [line 35][index.ts<2>] [35mnotes: ", notes);
+};
+
+const syncNote = (filePath: string): void => {
+  // TODO: master check if path and token exist before start main operations
+  const note = collectNoteFromFile(filePath);
+  sendNotes([note]);
+};
+
+const syncNotes = (dirPath: string): void => {
+  const notes = collectNotesFromDir(dirPath);
+  sendNotes(notes);
+};
+
+const collectNotes = (path: string): void => {
+  const { isDirectory } = statSync(path);
+  const fn = isDirectory ? syncNotes : syncNote;
+  fn(path);
+};
+
+const syncPath = process.argv[2];
+console.log("🦄: [line 54][index.ts<2>] [35msyncPath: ", syncPath);
+
+collectNotes(syncPath);
